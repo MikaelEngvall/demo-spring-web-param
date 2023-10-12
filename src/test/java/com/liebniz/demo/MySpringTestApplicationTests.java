6cb113dc7e861654b23b0a9825596c8c2d58f6dc
Liebniz <anders.leif.loren@liebniz.com>
2023-10-12 17:02:33 +0200

    Initial commit

---
+++ src/test/java/com/liebniz/demo/MySpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liebniz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
